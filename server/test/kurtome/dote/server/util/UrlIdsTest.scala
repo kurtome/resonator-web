cf00a8d3ba94282f625fe49f074582d22ea878c6
kurtome <kurt@melby.me>
2017-12-16 17:13:19 -0500

    fixing UrlIds tests (intentionally broke compatibility with previous UrlId change

--- server/test/kurtome/dote/server/util/UrlIdsTest.scala
+++ server/test/kurtome/dote/server/util/UrlIdsTest.scala
@@ -1,35 +1,36 @@
 package kurtome.dote.server.util
 
+import kurtome.dote.server.util.UrlIds.IdKinds
 import org.scalatest.WordSpec
 
 class UrlIdsTest extends WordSpec {
 
   "encode" when {
     "single digit ID" should {
-      "be length 8" in {
-        assert(UrlIds.encode(1).size === 6)
+      "be length 6" in {
+        assert(UrlIds.encode(IdKinds.Dotable, 1).size === 6)
       }
       "encode 1 the same each time" in {
-        assert(UrlIds.encode(1) === "AaBzaZ")
-        assert(UrlIds.encode(1) === "AaBzaZ")
+        assert(UrlIds.encode(IdKinds.Dotable, 1) === "aMxhJq")
+        assert(UrlIds.encode(IdKinds.Dotable, 1) === "aMxhJq")
       }
       "encode 2 the same each time" in {
-        assert(UrlIds.encode(2) === "NaMNEr")
-        assert(UrlIds.encode(2) === "NaMNEr")
+        assert(UrlIds.encode(IdKinds.Dotable, 2) === "aDrhrX")
+        assert(UrlIds.encode(IdKinds.Dotable, 2) === "aDrhrX")
       }
       "encode 3 the same each time" in {
-        assert(UrlIds.encode(3) === "3aD6XO")
-        assert(UrlIds.encode(3) === "3aD6XO")
+        assert(UrlIds.encode(IdKinds.Dotable, 3) === "qZDhGE")
+        assert(UrlIds.encode(IdKinds.Dotable, 3) === "qZDhGE")
       }
     }
     "Long ID" should {
       val longId: Long = Int.MaxValue.toLong + 1L
-      "be length 8" in {
-        assert(UrlIds.encode(longId).size === 7)
+      "be length 9" in {
+        assert(UrlIds.encode(IdKinds.Dotable, longId).size === 9)
       }
       "Long ID the same each time" in {
-        assert(UrlIds.encode(longId) === "Z8WBBMd")
-        assert(UrlIds.encode(longId) === "Z8WBBMd")
+        assert(UrlIds.encode(IdKinds.Dotable, longId) === "16hrN66Ay")
+        assert(UrlIds.encode(IdKinds.Dotable, longId) === "16hrN66Ay")
       }
     }
   }
@@ -37,18 +38,19 @@ class UrlIdsTest extends WordSpec {
   "decode" when {
     "single digit ID" should {
       "decode 1 from constant" in {
-        assert(UrlIds.decode("AaBzaZ") === 1)
+        assert(UrlIds.decode(IdKinds.Dotable, "aMxhJq") === 1)
       }
     }
     "reflexive with encode" should {
       "514356658" in {
-        assert(UrlIds.decode(UrlIds.encode(514356658)) === 514356658)
+        assert(
+          UrlIds.decode(IdKinds.Dotable, UrlIds.encode(IdKinds.Dotable, 514356658)) === 514356658)
       }
       "9" in {
-        assert(UrlIds.decode(UrlIds.encode(9)) === 9)
+        assert(UrlIds.decode(IdKinds.Dotable, UrlIds.encode(IdKinds.Dotable, 9)) === 9)
       }
       "100" in {
-        assert(UrlIds.decode(UrlIds.encode(100)) === 100)
+        assert(UrlIds.decode(IdKinds.Dotable, UrlIds.encode(IdKinds.Dotable, 100)) === 100)
       }
     }
   }
