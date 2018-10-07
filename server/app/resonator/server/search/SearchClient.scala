8047684c4ce7c6c02b64fe6ac466c48fdabc9980
kurtome <kurt@melby.me>
2018-10-07 15:17:40 -0400

    removing ElasticSearch functionality

--- server/app/resonator/server/search/SearchClient.scala
+++ server/app/resonator/server/search/SearchClient.scala
@@ -82,8 +82,8 @@ class SearchClient @Inject()(configuration: Configuration)(implicit ec: Executio
   private val esSsl =
     configuration.get[String]("resonator.search.elasticsearch.ssl")
 
-  private val clientUri =
-    ElasticsearchClientUri(null, List((esUrl, esPort)), Map("ssl" -> esSsl))
+//  private val clientUri =
+//    ElasticsearchClientUri(null, List((esUrl, esPort)), Map("ssl" -> esSsl))
 
   lazy val provider = {
     val provider = new BasicCredentialsProvider
@@ -91,233 +91,239 @@ class SearchClient @Inject()(configuration: Configuration)(implicit ec: Executio
     provider.setCredentials(AuthScope.ANY, credentials)
     provider
   }
-  val client = HttpClient(
-    clientUri,
-    new RequestConfigCallback {
-      override def customizeRequestConfig(requestConfigBuilder: Builder) = {
-        requestConfigBuilder
-      }
-    },
-    new HttpClientConfigCallback {
-      override def customizeHttpClient(httpClientBuilder: HttpAsyncClientBuilder) = {
-        httpClientBuilder.setDefaultCredentialsProvider(provider)
-      }
-    }
-  )
+//  val client = HttpClient(
+//    clientUri,
+//    new RequestConfigCallback {
+//      override def customizeRequestConfig(requestConfigBuilder: Builder) = {
+//        requestConfigBuilder
+//      }
+//    },
+//    new HttpClientConfigCallback {
+//      override def customizeHttpClient(httpClientBuilder: HttpAsyncClientBuilder) = {
+//        httpClientBuilder.setDefaultCredentialsProvider(provider)
+//      }
+//    }
+//  )
 
   private val dotablesIndex = "dotables-v2"
   private val docType = "_doc"
 
   // This cannot alter existing mappings, only create new ones.
   private def ensureIndexSchema(index: String) = {
-    client.execute {
-      createIndex(index)
-    } flatMap { _ =>
-      client.execute {
-        putMapping(index / docType) fields (
-          objectField("dotable").dynamic(false),
-          objectField("dotable.data").enabled(false),
-          keywordField("dotable.id"),
-          keywordField("dotable.kind"),
-          keywordField("dotable.slug"),
-          objectField("parent").dynamic(false),
-          objectField("parent.data").enabled(false),
-          keywordField("parent.id"),
-          keywordField("parent.kind"),
-          keywordField("parent.slug"),
-          objectField("indexedFields").dynamic(false),
-          textField("indexedFields.title"),
-          textField("indexedFields.parentTitle"),
-          textField("indexedFields.combinedText").termVector("yes").analyzer("english"),
-          keywordField("indexedFields.tagIds"),
-          textField("indexedFields.tagDisplayValues")
-        )
-
-      }
-    }
+    Future.unit
+//    client.execute {
+//      createIndex(index)
+//    } flatMap { _ =>
+//      client.execute {
+//        putMapping(index / docType) fields (
+//          objectField("dotable").dynamic(false),
+//          objectField("dotable.data").enabled(false),
+//          keywordField("dotable.id"),
+//          keywordField("dotable.kind"),
+//          keywordField("dotable.slug"),
+//          objectField("parent").dynamic(false),
+//          objectField("parent.data").enabled(false),
+//          keywordField("parent.id"),
+//          keywordField("parent.kind"),
+//          keywordField("parent.slug"),
+//          objectField("indexedFields").dynamic(false),
+//          textField("indexedFields.title"),
+//          textField("indexedFields.parentTitle"),
+//          textField("indexedFields.combinedText").termVector("yes").analyzer("english"),
+//          keywordField("indexedFields.tagIds"),
+//          textField("indexedFields.tagDisplayValues")
+//        )
+//
+//      }
+//    }
   }
 
   ensureIndexSchema(dotablesIndex)
 
   def indexDotables(dotables: Seq[Dotable]): Future[Unit] = {
-    val toIndex = dotables
-      .filter(_.kind match {
-        case Dotable.Kind.PODCAST => true
-        case Dotable.Kind.PODCAST_EPISODE => true
-        case _ => false
-      })
-
-    if (toIndex.nonEmpty) {
-      client.execute {
-        bulk(
-          toIndex.map(
-            dotable =>
-              indexInto(dotablesIndex / docType)
-                .id(dotable.id)
-                .doc(extractDataDoc(dotable)))
-        )
-      } map {
-        case Right(requestSuccess) => Unit
-        case Left(requestFailure) => warn(requestFailure.body)
-      } map (_ => Unit)
-    } else {
-      Future.unit
-    }
+    Future.unit
+//    val toIndex = dotables
+//      .filter(_.kind match {
+//        case Dotable.Kind.PODCAST => true
+//        case Dotable.Kind.PODCAST_EPISODE => true
+//        case _ => false
+//      })
+//
+//    if (toIndex.nonEmpty) {
+//      client.execute {
+//        bulk(
+//          toIndex.map(
+//            dotable =>
+//              indexInto(dotablesIndex / docType)
+//                .id(dotable.id)
+//                .doc(extractDataDoc(dotable)))
+//        )
+//      } map {
+//        case Right(requestSuccess) => Unit
+//        case Left(requestFailure) => warn(requestFailure.body)
+//      } map (_ => Unit)
+//    } else {
+//      Future.unit
+//    }
   }
 
   def searchAll(query: String, offset: Int = 0, limit: Int = 30): Future[Seq[Dotable]] = {
-    client.execute {
-      search(dotablesIndex)
-        .from(offset)
-        .size(limit)
-        .query(
-          boolQuery()
-            .filter(
-              matchQuery("indexedFields.combinedText", query)
-                .minimumShouldMatch("3<70%")
-                .fuzziness("AUTO")
-                .prefixLength(3)
-            )
-            .should(
-              matchPhraseQuery("indexedFields.combinedText", query)
-                .boost(2),
-              matchQuery("indexedFields.combinedText", query)
-                .minimumShouldMatch("100%")
-                .boost(1)
-                .fuzziness("AUTO")
-                .prefixLength(3),
-              matchQuery("indexedFields.parentTitle", query)
-                .boost(1),
-              matchQuery("indexedFields.tagDisplayValues", query)
-                .boost(50),
-              boolQuery()
-                .must(
-                  matchPhraseQuery("indexedFields.title", query),
-                  termQuery("dotable.kind", "PODCAST")
-                )
-                .boost(100)
-            )
-        )
-    } map {
-      case Right(requestSuccess) => {
-        requestSuccess.result.hits.hits.map(resultData => {
-          val indexedData = JsonFormat.fromJsonString[SearchIndexedData](resultData.sourceAsString)
-          parseDataDoc(indexedData)
-        })
-      }
-      case Left(requestFailure) => {
-        warn(requestFailure.body)
-        Nil
-      }
-    }
+    Future(Nil)
+//    client.execute {
+//      search(dotablesIndex)
+//        .from(offset)
+//        .size(limit)
+//        .query(
+//          boolQuery()
+//            .filter(
+//              matchQuery("indexedFields.combinedText", query)
+//                .minimumShouldMatch("3<70%")
+//                .fuzziness("AUTO")
+//                .prefixLength(3)
+//            )
+//            .should(
+//              matchPhraseQuery("indexedFields.combinedText", query)
+//                .boost(2),
+//              matchQuery("indexedFields.combinedText", query)
+//                .minimumShouldMatch("100%")
+//                .boost(1)
+//                .fuzziness("AUTO")
+//                .prefixLength(3),
+//              matchQuery("indexedFields.parentTitle", query)
+//                .boost(1),
+//              matchQuery("indexedFields.tagDisplayValues", query)
+//                .boost(50),
+//              boolQuery()
+//                .must(
+//                  matchPhraseQuery("indexedFields.title", query),
+//                  termQuery("dotable.kind", "PODCAST")
+//                )
+//                .boost(100)
+//            )
+//        )
+//    } map {
+//      case Right(requestSuccess) => {
+//        requestSuccess.result.hits.hits.map(resultData => {
+//          val indexedData = JsonFormat.fromJsonString[SearchIndexedData](resultData.sourceAsString)
+//          parseDataDoc(indexedData)
+//        })
+//      }
+//      case Left(requestFailure) => {
+//        warn(requestFailure.body)
+//        Nil
+//      }
+//    }
   }
 
   def searchPodcast(query: String, offset: Int = 0, limit: Int = 30): Future[Seq[Dotable]] = {
-    client.execute {
-      search(dotablesIndex)
-        .from(offset)
-        .size(limit)
-        .query(
-          boolQuery()
-            .must(
-              matchQuery("indexedFields.combinedText", query)
-                .minimumShouldMatch("90%")
-                .fuzziness("AUTO")
-                .prefixLength(3))
-            .should(
-              matchQuery("indexedFields.title", query)
-                .minimumShouldMatch("60%")
-                .boost(200)
-            )
-            .filter(termQuery("dotable.kind", "PODCAST"))
-        )
-    } map {
-      case Right(requestSuccess) => {
-        requestSuccess.result.hits.hits.map(resultData => {
-          val indexedData = JsonFormat.fromJsonString[SearchIndexedData](resultData.sourceAsString)
-          parseDataDoc(indexedData)
-        })
-      }
-      case Left(requestFailure) => {
-        warn(requestFailure.body)
-        Nil
-      }
-    }
+    Future(Nil)
+//    client.execute {
+//      search(dotablesIndex)
+//        .from(offset)
+//        .size(limit)
+//        .query(
+//          boolQuery()
+//            .must(
+//              matchQuery("indexedFields.combinedText", query)
+//                .minimumShouldMatch("90%")
+//                .fuzziness("AUTO")
+//                .prefixLength(3))
+//            .should(
+//              matchQuery("indexedFields.title", query)
+//                .minimumShouldMatch("60%")
+//                .boost(200)
+//            )
+//            .filter(termQuery("dotable.kind", "PODCAST"))
+//        )
+//    } map {
+//      case Right(requestSuccess) => {
+//        requestSuccess.result.hits.hits.map(resultData => {
+//          val indexedData = JsonFormat.fromJsonString[SearchIndexedData](resultData.sourceAsString)
+//          parseDataDoc(indexedData)
+//        })
+//      }
+//      case Left(requestFailure) => {
+//        warn(requestFailure.body)
+//        Nil
+//      }
+//    }
   }
 
   def searchEpisode(query: String, offset: Int = 0, limit: Int = 30): Future[Seq[Dotable]] = {
-    client.execute {
-      search(dotablesIndex)
-        .from(offset)
-        .size(limit)
-        .query(
-          boolQuery()
-            .must(
-              matchQuery("indexedFields.combinedText", query)
-                .minimumShouldMatch("100%")
-                .fuzziness("AUTO")
-                .prefixLength(3)
-            )
-            .should(
-              matchQuery("indexedFields.parentTitle", query)
-                .minimumShouldMatch("60%")
-                .boost(100)
-            )
-            .filter(termQuery("dotable.kind", "PODCAST_EPISODE"))
-        )
-    } map {
-      case Right(requestSuccess) => {
-        requestSuccess.result.hits.hits.map(resultData => {
-          val indexedData = JsonFormat.fromJsonString[SearchIndexedData](resultData.sourceAsString)
-          parseDataDoc(indexedData)
-        })
-      }
-      case Left(requestFailure) => {
-        warn(requestFailure.body)
-        Nil
-      }
-    }
+    Future(Nil)
+//    client.execute {
+//      search(dotablesIndex)
+//        .from(offset)
+//        .size(limit)
+//        .query(
+//          boolQuery()
+//            .must(
+//              matchQuery("indexedFields.combinedText", query)
+//                .minimumShouldMatch("100%")
+//                .fuzziness("AUTO")
+//                .prefixLength(3)
+//            )
+//            .should(
+//              matchQuery("indexedFields.parentTitle", query)
+//                .minimumShouldMatch("60%")
+//                .boost(100)
+//            )
+//            .filter(termQuery("dotable.kind", "PODCAST_EPISODE"))
+//        )
+//    } map {
+//      case Right(requestSuccess) => {
+//        requestSuccess.result.hits.hits.map(resultData => {
+//          val indexedData = JsonFormat.fromJsonString[SearchIndexedData](resultData.sourceAsString)
+//          parseDataDoc(indexedData)
+//        })
+//      }
+//      case Left(requestFailure) => {
+//        warn(requestFailure.body)
+//        Nil
+//      }
+//    }
   }
 
   def moreLike(dotable: Dotable, offset: Int = 0, limit: Int = 24): Future[Seq[Dotable]] = {
-    val tags = dotable.kind match {
-      case Dotable.Kind.PODCAST => dotable.getTagCollection.tags
-      case _ => dotable.getRelatives.getParent.getTagCollection.tags
-    }
-    val combinedText = extractIndexStruct(dotable).getIndexedFields.combinedText
-    if (tags.nonEmpty) {
-      client.execute {
-        search(dotablesIndex)
-          .from(offset)
-          .size(limit)
-          .query(
-            boolQuery()
-              .filter(
-                termQuery("dotable.kind", dotable.kind.toString)
-              )
-              .must(
-                moreLikeThisQuery("indexedFields.combinedText")
-                  .likeTexts(combinedText)
-                  .stopWords(stopWords)
-              )
-          )
-          .postFilter(not(termQuery("dotable.id", dotable.id)))
-      } map {
-        case Right(requestSuccess) => {
-          requestSuccess.result.hits.hits.map(resultData => {
-            val indexedData =
-              JsonFormat.fromJsonString[SearchIndexedData](resultData.sourceAsString)
-            parseDataDoc(indexedData)
-          })
-        }
-        case Left(requestFailure) => {
-          warn(requestFailure.body)
-          Nil
-        }
-      }
-    } else {
-      Future(Nil)
-    }
+    Future(Nil)
+//    val tags = dotable.kind match {
+//      case Dotable.Kind.PODCAST => dotable.getTagCollection.tags
+//      case _ => dotable.getRelatives.getParent.getTagCollection.tags
+//    }
+//    val combinedText = extractIndexStruct(dotable).getIndexedFields.combinedText
+//    if (tags.nonEmpty) {
+//      client.execute {
+//        search(dotablesIndex)
+//          .from(offset)
+//          .size(limit)
+//          .query(
+//            boolQuery()
+//              .filter(
+//                termQuery("dotable.kind", dotable.kind.toString)
+//              )
+//              .must(
+//                moreLikeThisQuery("indexedFields.combinedText")
+//                  .likeTexts(combinedText)
+//                  .stopWords(stopWords)
+//              )
+//          )
+//          .postFilter(not(termQuery("dotable.id", dotable.id)))
+//      } map {
+//        case Right(requestSuccess) => {
+//          requestSuccess.result.hits.hits.map(resultData => {
+//            val indexedData =
+//              JsonFormat.fromJsonString[SearchIndexedData](resultData.sourceAsString)
+//            parseDataDoc(indexedData)
+//          })
+//        }
+//        case Left(requestFailure) => {
+//          warn(requestFailure.body)
+//          Nil
+//        }
+//      }
+//    } else {
+//      Future(Nil)
+//    }
   }
 
   private def extractType(dotable: Dotable): String = {
